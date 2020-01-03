bb02c7aec5f085721e1c0181cd3561823f0c4797
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-01-03 18:46:13 +0500

    Improve logging

--- src/clients/vk.ts
+++ src/clients/vk.ts
@@ -67,7 +67,8 @@ const vkMiddleware: Middleware = async ctx => {
   try {
     vkCallback = await vkCallbackDecoder.decodePromise(ctx.request.body)
   } catch (e) {
-    console.log(`Invalid body format recieved:\n${ctx.body}`)
+    console.log(`Invalid body format recieved:\n${e}`)
+    console.log(`Recieved body:\n${ctx.request.body}`)
     return
   }
 
@@ -98,6 +99,7 @@ const vkMiddleware: Middleware = async ctx => {
       newMessage = await vkCallbackEventMessageNewDecoder.decodePromise(ctx.request.body)
     } catch (e) {
       console.log(`Invalid body format recieved (message_new format expected):\n${e}`)
+      console.log(`Recieved body:\n${ctx.request.body}`)
       return
     }
 
